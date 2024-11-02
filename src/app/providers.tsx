1a58b4b199e1eb097930986e0faa2432006ab66f
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-11-03 01:20:29 +0530

    chore: update providers to use new config

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,23 +1,21 @@
-'use client'
+"use client";
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { type ReactNode, useState } from 'react'
-import { type State, WagmiProvider } from 'wagmi'
-
-import { getConfig } from '@/wagmi'
+import { config } from "@/wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { type ReactNode, useState } from "react";
+import { type State, WagmiProvider } from "wagmi";
 
 export function Providers(props: {
-  children: ReactNode
-  initialState?: State
+    children: ReactNode;
+    initialState?: State;
 }) {
-  const [config] = useState(() => getConfig())
-  const [queryClient] = useState(() => new QueryClient())
+    const [queryClient] = useState(() => new QueryClient());
 
-  return (
-    <WagmiProvider config={config} initialState={props.initialState}>
-      <QueryClientProvider client={queryClient}>
-        {props.children}
-      </QueryClientProvider>
-    </WagmiProvider>
-  )
+    return (
+        <WagmiProvider config={config} initialState={props.initialState}>
+            <QueryClientProvider client={queryClient}>
+                {props.children}
+            </QueryClientProvider>
+        </WagmiProvider>
+    );
 }
