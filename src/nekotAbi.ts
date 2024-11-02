78fadbea95ee782784d1afbe7ce2d1bee560a52e
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-11-02 17:51:08 +0530

    chore: update nekot abi

--- src/nekotAbi.ts
+++ src/nekotAbi.ts
@@ -1,4 +1,65 @@
 export const nekotAbi = [
+    // Read functions
+    {
+        name: "name",
+        type: "function",
+        stateMutability: "view",
+        inputs: [],
+        outputs: [{ type: "string" }],
+    },
+    {
+        name: "symbol",
+        type: "function",
+        stateMutability: "view",
+        inputs: [],
+        outputs: [{ type: "string" }],
+    },
+    {
+        name: "decimals",
+        type: "function",
+        stateMutability: "view",
+        inputs: [],
+        outputs: [{ type: "uint8" }],
+    },
+    {
+        name: "totalSupply",
+        type: "function",
+        stateMutability: "view",
+        inputs: [],
+        outputs: [{ type: "uint256" }],
+    },
+    {
+        name: "balanceOf",
+        type: "function",
+        stateMutability: "view",
+        inputs: [{ name: "account", type: "address" }],
+        outputs: [{ type: "uint256" }],
+    },
+    {
+        name: "allowance",
+        type: "function",
+        stateMutability: "view",
+        inputs: [
+            { name: "owner", type: "address" },
+            { name: "spender", type: "address" },
+        ],
+        outputs: [{ type: "uint256" }],
+    },
+    {
+        name: "owner",
+        type: "function",
+        stateMutability: "view",
+        inputs: [],
+        outputs: [{ type: "address" }],
+    },
+    {
+        name: "paused",
+        type: "function",
+        stateMutability: "view",
+        inputs: [],
+        outputs: [{ type: "bool" }],
+    },
+    // Write functions
     {
         name: "transfer",
         type: "function",
@@ -8,13 +69,6 @@ export const nekotAbi = [
             { name: "amount", type: "uint256" },
         ],
         outputs: [{ type: "bool" }],
-    },
-    {
-        name: "balanceOf",
-        type: "function",
-        stateMutability: "view",
-        inputs: [{ name: "account", type: "address" }],
-        outputs: [{ type: "uint256" }],
     },
     {
         name: "approve",
@@ -27,11 +81,83 @@ export const nekotAbi = [
         outputs: [{ type: "bool" }],
     },
     {
+        name: "transferFrom",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [
+            { name: "from", type: "address" },
+            { name: "to", type: "address" },
+            { name: "amount", type: "uint256" },
+        ],
+        outputs: [{ type: "bool" }],
+    },
+    {
+        name: "increaseAllowance",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [
+            { name: "spender", type: "address" },
+            { name: "addedValue", type: "uint256" },
+        ],
+        outputs: [{ type: "bool" }],
+    },
+    {
+        name: "decreaseAllowance",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [
+            { name: "spender", type: "address" },
+            { name: "subtractedValue", type: "uint256" },
+        ],
+        outputs: [{ type: "bool" }],
+    },
+    {
+        name: "mint",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [
+            { name: "to", type: "address" },
+            { name: "amount", type: "uint256" },
+        ],
+        outputs: [{ type: "bool" }],
+    },
+    {
         name: "burn",
         type: "function",
         stateMutability: "nonpayable",
         inputs: [{ name: "amount", type: "uint256" }],
         outputs: [{ type: "bool" }],
+    },
+    {
+        name: "pause",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [],
+        outputs: [],
+    },
+    {
+        name: "unpause",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [],
+        outputs: [],
+    },
+    {
+        name: "transferOwnership",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [{ name: "newOwner", type: "address" }],
+        outputs: [],
+    },
+    {
+        name: "recoverERC20",
+        type: "function",
+        stateMutability: "nonpayable",
+        inputs: [
+            { name: "tokenAddress", type: "address" },
+            { name: "tokenAmount", type: "uint256" },
+        ],
+        outputs: [],
     },
 ] as const;
 
