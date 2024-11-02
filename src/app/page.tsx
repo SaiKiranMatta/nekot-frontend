9c2b4ab046d7d6584f7bae209c30f490f59c0b63
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-11-02 19:28:32 +0530

    chore: add all components in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,11 @@ import { Account } from "@/components/Account";
 import { AllowanceView } from "@/components/token/AllowanceView";
 import { ApproveSpender } from "@/components/token/ApproveSpender";
 import { BurnToken } from "@/components/token/BurnToken";
+import { DecreaseAllowance } from "@/components/token/DecreaseAllowance";
+import { IncreaseAllowance } from "@/components/token/IncreaseAllowance";
+import { MintToken } from "@/components/token/MintToken";
+import { RecoverToken } from "@/components/token/RecoverToken";
+import { TokenControls } from "@/components/token/TokenControls";
 import { TokenInfo } from "@/components/token/TokenInfo";
 import { TransferFrom } from "@/components/token/TransferFrom";
 import { TransferToken } from "@/components/token/TransferToken";
@@ -19,7 +24,13 @@ function App() {
                 <div className=" flex flex-col space-y-4">
                     <Account />
                     <TokenInfo />
+
                     <ViewBalance address={address} />
+                    <MintToken />
+                    <TokenControls />
+                    <IncreaseAllowance />
+                    <RecoverToken />
+                    <DecreaseAllowance />
                     <TransferToken />
                     <ApproveSpender />
                     <TransferFrom />
