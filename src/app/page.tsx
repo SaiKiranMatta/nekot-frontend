0c45b6ae5e4b4def4d0b707a63722364de02753c
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-11-03 01:21:20 +0530

    feat: add tab view to contracts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,28 +15,43 @@ import { TransferToken } from "@/components/token/TransferToken";
 import { ViewBalance } from "@/components/token/ViewBalance";
 import { WalletOptions } from "@/components/WalletOptions";
 import { useAccount } from "wagmi";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import TokenBalanceViewer from "@/components/token/TokenBalanceViewer";
 
 function App() {
     const { isConnected, address } = useAccount();
     return (
-        <div className=" min-h-screen  max-w-screen flex flex-col justify-center items-center">
+        <div className=" min-h-screen  flex flex-col  items-center">
             {isConnected && address ? (
-                <div className=" flex flex-col space-y-4">
-                    <Account />
-                    <TokenInfo />
-
-                    <ViewBalance address={address} />
-                    <MintToken />
-                    <TokenControls />
-                    <IncreaseAllowance />
-                    <RecoverToken />
-                    <DecreaseAllowance />
-                    <TransferToken />
-                    <ApproveSpender />
-                    <TransferFrom />
-                    <AllowanceView />
-                    <BurnToken />
-                </div>
+                <Tabs defaultValue="account" className="w-full my-20 px-4">
+                    <TabsList className=" grid grid-cols-2 mb-8">
+                        <TabsTrigger value="account">Read Contract</TabsTrigger>
+                        <TabsTrigger value="password">
+                            Write Contract
+                        </TabsTrigger>
+                    </TabsList>
+                    <TabsContent value="account">
+                        <div className=" grid md:grid-cols-3 grid-cols-2 gap-4">
+                            <TokenInfo />
+                            <ViewBalance address={address} />
+                            <AllowanceView />
+                            <TokenBalanceViewer />
+                        </div>
+                    </TabsContent>
+                    <TabsContent value="password">
+                        <div className=" grid md:grid-cols-3 grid-cols-2 gap-4">
+                            <TransferToken />
+                            <TransferFrom />
+                            <RecoverToken />
+                            <ApproveSpender />
+                            <IncreaseAllowance />
+                            <DecreaseAllowance />
+                            <MintToken />
+                            <TokenControls />
+                            <BurnToken />
+                        </div>
+                    </TabsContent>
+                </Tabs>
             ) : (
                 <WalletOptions />
             )}
