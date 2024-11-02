a42974a5fb31226553dd64a905c63410805bd1a8
SaiKiranMatta <saikirangoud.matta@gmail.com>
2024-11-03 01:16:09 +0530

    chore: update wagmi config

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,26 +2,20 @@ import { http, cookieStorage, createConfig, createStorage } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
 
-export function getConfig() {
-    return createConfig({
-        chains: [sepolia],
-        connectors: [
-            // injected(),
-            coinbaseWallet(),
-            walletConnect({ projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID }),
-        ],
-        storage: createStorage({
-            storage: cookieStorage,
+export const config = createConfig({
+    chains: [sepolia],
+    connectors: [
+        // injected(),
+        coinbaseWallet(),
+        walletConnect({
+            projectId: process.env.NEXT_PUBLIC_WC_PROJECT_ID as string,
         }),
-        ssr: true,
-        transports: {
-            [sepolia.id]: http(),
-        },
-    });
-}
-
-declare module "wagmi" {
-    interface Register {
-        config: ReturnType<typeof getConfig>;
-    }
-}
+    ],
+    storage: createStorage({
+        storage: cookieStorage,
+    }),
+    ssr: true,
+    transports: {
+        [sepolia.id]: http(),
+    },
+});
